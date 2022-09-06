d23dad8bd5bf723e704ee4c87d8277c039241fd5
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-09-06 22:42:10 +0200

    Add hexa

--- js/convert.js
+++ js/convert.js
@@ -1,52 +1,100 @@
 const input = document.getElementById('input');
 const result = document.getElementById('result');
 const type = document.getElementById('type');
+const letters = new Map();
+
+letters.set('A', 10);
+letters.set('B', 11);
+letters.set('C', 12);
+letters.set('D', 13);
+letters.set('E', 14);
+letters.set('F', 15);
 
 /**
- * Convert decimal number to binary number
- * @param {string} decimal number decimal to convert in binary
+ * Convert hexadecimal number to decimal number
+ * @param {string} hexadecimal hexadecimal number to convert in decimal
  */
-const toBinary = (decimal) => {
-    for (let i = 0; i < decimal.length; i++) {
-        if (!(decimal[i]).match(/[0-9]/)) {
+const hexadecimalToDecimal = (hexadecimal) => {
+
+    hexadecimal = hexadecimal
+        .toUpperCase()
+        .replace('0X', '');
+    
+    for (let letter of hexadecimal) {
+        if (!letters.has(letter)) {
             return 0;
         }
     }
-    let binary = "";
+    let decimal = 0;
+
+    hexadecimal
+        .split('')
+        .reverse()
+        .forEach((e, i) => decimal += Math.pow(16, i) * letters.get(e));
+
+    return decimal;
+}
+
+/**
+ * Convert decimal number to binary number
+ * @param {string} decimal decimal number to convert in binary
+ */
+const decimalToBinary = (decimal) => {
+
+    for (let letter of decimal) {
+        if (!(letter).match(/[0-9]/)) {
+            return 0;
+        }
+    }
+    let binary = '';
+
     while (decimal >  0) {
         binary += decimal % 2;
         decimal = parseInt(decimal / 2);
     }
-    return binary.split('').reverse().join('');
+    return binary
+            .split('')
+            .reverse()
+            .join('');
 }
 
 /**
  * Convert binary to decimal number
  * @param {string} binary number binary to convert in decimal
  */
-const toDecimal = (binary) => {
-    for (let i = 0; i < binary.length; i++) {
-        if (!['0','1'].includes(binary[i])) {
+const binaryToDecimal = (binary) => {
+
+    for (let letter of decimal) {
+        if (!['0','1'].includes(letter)) {
             return 0;
         }
-    } 
+    }
     let decimal = 0;
-    binary.split('').reverse().forEach((e, i) => { e === '1' ? decimal += Math.pow(2,i) : 0});
+
+    binary
+        .split('')
+        .reverse()
+        .forEach((e, i) => { e === '1' ? decimal += Math.pow(2, i) : 0});
+
     return decimal;
 }
 
-/**
- * Launch convert
- */
 const convert = () => {
+
     if (input.value.length !== 0) {
-        if (type.value === 'decimal') {
-            result.value = toDecimal(input.value);
-        } else if (type.value === 'binary') {
-            result.value = toBinary(input.value);
+        switch (type.value) {
+            case 'decimal': 
+                result.value = binaryToDecimal(input.value);
+                break;
+            case 'binary':
+                result.value = decimalToBinary(input.value);
+                break;
+            case 'hexadecimal':
+                result.value = hexadecimalToDecimal(input.value);
+                break;
         }
     }
 }
 
-input.addEventListener('input',convert);
-type.addEventListener('change',convert);
+input.addEventListener('input', convert);
+type.addEventListener('change', convert);
